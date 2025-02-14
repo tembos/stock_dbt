c6f54d459f1f10d5fbf22f0e704f446fa2f553ef
jean legrais <jeanlegrais@jeans-MacBook-Pro.local>
2025-02-14 06:02:54 -0500

    edit dockerfile

--- airflow-composer/dags/nvda_dag_api_to_bq4.py
+++ airflow-composer/dags/nvda_dag_api_to_bq4.py
@@ -13,7 +13,7 @@ from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperato
 from google.cloud import secretmanager
 import pendulum
 
-SYMBOL = "NVDA" #add
+SYMBOL = "NVDA" #add k
 
 # GCP variables from composer env
 BIGQUERY_PROJECT = "bigquerysheets-404104"
